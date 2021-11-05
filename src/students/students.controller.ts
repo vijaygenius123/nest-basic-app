79e803bdac0925db3f69792fff445f63e7914013
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-11-05 09:05:12 +0000

    Add Methods For Creating And Updating Student

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 
 @Controller('students')
 class StudentsController {
@@ -12,6 +12,17 @@ class StudentsController {
   getStudent(@Param("studentId") studentId: string){
     return 'Getting Student ' + studentId
   }
+
+  @Post('')
+  createStudent(@Body() body){
+    return 'Create Student'
+  }
+
+  @Put('/:studentId')
+  updateStudent(@Param() studentId: string, @Body() body){
+    return 'Update Student'
+  }
+
 
 }
 
