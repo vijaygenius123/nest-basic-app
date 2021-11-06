b4783a647d97be0cf332117361be9eefd9182454
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-11-06 21:06:51 +0000

    Create DTO Student student.dto.ts

---
+++ src/students/student.dto.ts
@@ -0,0 +1,10 @@
+export class StudentResponseDTO {
+  id: string;
+  name: string;
+  teacher: string;
+}
+
+export class CreateStudentDTO {
+  name: string;
+  teacher: string;
+}
--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,20 +1,21 @@
 import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
+import { CreateStudentDTO, StudentResponseDTO } from "./student.dto";
 
 @Controller('students')
 class StudentsController {
 
   @Get('')
-  getStudents(){
+  getStudents(): StudentResponseDTO[]{
     return 'All Students'
   }
 
   @Get('/:studentId')
-  getStudent(@Param("studentId") studentId: string){
+  getStudent(@Param("studentId") studentId: string): StudentResponseDTO{
     return 'Getting Student ' + studentId
   }
 
   @Post('')
-  createStudent(@Body() body){
+  createStudent(@Body() body: CreateStudentDTO){
     return 'Create Student'
   }
 
